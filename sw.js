66c1f8d5965d557ff10fe8fe8e7f0d29955098ea
TheRhyno <theo38das38@gmail.com>
2026-05-26 22:19:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  ⚡ Pour déclencher une mise à jour : changer APP_VERSION
 // ============================================================
 
-const APP_VERSION  = 'v1.0.10';
+const APP_VERSION  = 'v1.0.11';
 const CACHE_NAME   = `rhynozic-shell-${APP_VERSION}`;
 const AUDIO_CACHE  = `rhynozic-audio-${APP_VERSION}`;
 const IMG_CACHE    = `rhynozic-img-${APP_VERSION}`;
